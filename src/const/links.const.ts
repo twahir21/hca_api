32eb16b53424d71311d3c1c0d249ee3a806f2b04
twahir21 <twahirsudy3@gmail.com>
2025-12-28 01:56:30 +0300

    adding neon true

--- src/const/links.const.ts
+++ src/const/links.const.ts
@@ -1,11 +1,11 @@
-export const isProduction = true;
+export const isProduction = false;
 
 export const links = {
     serverLink: isProduction ? "https://api.highercareer.academy" : "http://localhost:8080",
     clientLink: isProduction ? "https://accounts.highercareer.academy" : "http://localhost:5173",
 }
 
-const isNeon = false;
+const isNeon = true;
 
 export const dbLink = isNeon
   ? (isProduction
